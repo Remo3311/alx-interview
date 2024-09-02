7a2b56e7dcf655acf95cf4b94d517a1d7380ba5a
Remo3311 <remo33553311@gmail.com>
2024-09-02 17:36:30 +0000

    sass

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,54 @@
 #!/usr/bin/python3
+"""Island Perimeter - ALX Interview"""
+
+
+def check_val(x):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if (x == 0):
+        return 1
+    return 0
+
+
 def island_perimeter(grid):
-    perimeter = 0
-    
-    rows = len(grid)
-    cols = len(grid[0])
-    
-    for i in range(rows):
-        for j in range(cols):
+    """_summary_
+
+    Args:
+        grid (_type_): _description_
+    """
+
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+
+    x = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
-                # Start with 4 sides for each land cell
-                perimeter += 4
-                
-                # Check the cell above (i-1, j)
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 1
-                
-                # Check the cell below (i+1, j)
-                if i < rows - 1 and grid[i+1][j] == 1:
-                    perimeter -= 1
-                
-                # Check the cell to the left (i, j-1)
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 1
-                
-                # Check the cell to the right (i, j+1)
-                if j < cols - 1 and grid[i][j+1] == 1:
-                    perimeter -= 1
-    
-    return perimeter
+                if i-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i-1][j])
+                if j-1 < 0:
+                    x += 1
+                else:
+                    x += check_val(grid[i][j-1])
+
+                try:
+                    x += check_val(grid[i+1][j])
+                except IndexError:
+                    x += 1
+                try:
+                    x += check_val(grid[i][j+1])
+                except IndexError:
+                    x += 1
+
+    return x
